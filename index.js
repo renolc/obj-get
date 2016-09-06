fdb39bd232567241c9faecfed5c02d985a9468ef
Phillip Gibson <pgibson@carekinesis.com>
2016-09-06 11:52:07 -0400

    Simplify array access

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ module.exports = (value, ...props) => props
   .reduce((arr, prop) => arr.concat(prop.split('.')), [])
   .reduce((arr, prop) => {
     const arrayAccess = prop.match(/(.*)\[(.*)\]/)
-    return arr.concat((arrayAccess) ? [arrayAccess[1], `[${arrayAccess[2]}]`] : prop)
+    return arr.concat((arrayAccess) ? [arrayAccess[1], arrayAccess[2]] : prop)
   }, [])
-  .reduce((obj, prop) => {
-    if (prop.startsWith('[')) prop = prop.slice(1, prop.length-1)
-    return (obj) ? obj[prop] : undefined
-  }, value)
+  .reduce((obj, prop) => (obj) ? obj[prop] : undefined, value)
